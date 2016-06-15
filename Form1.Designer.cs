01ab3dd47b277dffd25d8833a4b35ea218a8f83b
dyckd <dyckd@my.erau.edu>
2016-06-15 16:58:15 -0700

    Import

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Daniels_LightTestApplication
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_runtest = new System.Windows.Forms.Button();
+            this.label_lightsensor_baudrate = new System.Windows.Forms.Label();
+            this.lightsensor_baudrate_db = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lightsensor_databits_db = new System.Windows.Forms.ComboBox();
+            this.lightsensor_parity_label = new System.Windows.Forms.Label();
+            this.lightsensor_parity_db = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btn_runtest
+            // 
+            this.btn_runtest.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_runtest.Location = new System.Drawing.Point(12, 12);
+            this.btn_runtest.Name = "btn_runtest";
+            this.btn_runtest.Size = new System.Drawing.Size(75, 23);
+            this.btn_runtest.TabIndex = 0;
+            this.btn_runtest.Text = "&Run Test";
+            this.btn_runtest.UseVisualStyleBackColor = false;
+            this.btn_runtest.Click += new System.EventHandler(this.btn_runtest_Click);
+            // 
+            // label_lightsensor_baudrate
+            // 
+            this.label_lightsensor_baudrate.AutoSize = true;
+            this.label_lightsensor_baudrate.Location = new System.Drawing.Point(12, 39);
+            this.label_lightsensor_baudrate.Name = "label_lightsensor_baudrate";
+            this.label_lightsensor_baudrate.Size = new System.Drawing.Size(112, 13);
+            this.label_lightsensor_baudrate.TabIndex = 1;
+            this.label_lightsensor_baudrate.Text = "Light Sensor Baudrate";
+            // 
+            // lightsensor_baudrate_db
+            // 
+            this.lightsensor_baudrate_db.AllowDrop = true;
+            this.lightsensor_baudrate_db.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lightsensor_baudrate_db.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lightsensor_baudrate_db.FormattingEnabled = true;
+            this.lightsensor_baudrate_db.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.lightsensor_baudrate_db.Location = new System.Drawing.Point(130, 36);
+            this.lightsensor_baudrate_db.Name = "lightsensor_baudrate_db";
+            this.lightsensor_baudrate_db.Size = new System.Drawing.Size(84, 21);
+            this.lightsensor_baudrate_db.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Light Sensor Databits";
+            // 
+            // lightsensor_databits_db
+            // 
+            this.lightsensor_databits_db.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lightsensor_databits_db.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lightsensor_databits_db.FormattingEnabled = true;
+            this.lightsensor_databits_db.Items.AddRange(new object[] {
+            "7",
+            "8",
+            "9"});
+            this.lightsensor_databits_db.Location = new System.Drawing.Point(130, 63);
+            this.lightsensor_databits_db.Name = "lightsensor_databits_db";
+            this.lightsensor_databits_db.Size = new System.Drawing.Size(84, 21);
+            this.lightsensor_databits_db.TabIndex = 4;
+            // 
+            // lightsensor_parity_label
+            // 
+            this.lightsensor_parity_label.AutoSize = true;
+            this.lightsensor_parity_label.Location = new System.Drawing.Point(12, 93);
+            this.lightsensor_parity_label.Name = "lightsensor_parity_label";
+            this.lightsensor_parity_label.Size = new System.Drawing.Size(95, 13);
+            this.lightsensor_parity_label.TabIndex = 6;
+            this.lightsensor_parity_label.Text = "Light Sensor Parity";
+            // 
+            // lightsensor_parity_db
+            // 
+            this.lightsensor_parity_db.AllowDrop = true;
+            this.lightsensor_parity_db.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lightsensor_parity_db.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lightsensor_parity_db.FormattingEnabled = true;
+            this.lightsensor_parity_db.Items.AddRange(new object[] {
+            "Even",
+            "Odd",
+            "None"});
+            this.lightsensor_parity_db.Location = new System.Drawing.Point(130, 90);
+            this.lightsensor_parity_db.Name = "lightsensor_parity_db";
+            this.lightsensor_parity_db.Size = new System.Drawing.Size(84, 21);
+            this.lightsensor_parity_db.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 183);
+            this.Controls.Add(this.lightsensor_parity_db);
+            this.Controls.Add(this.lightsensor_parity_label);
+            this.Controls.Add(this.lightsensor_databits_db);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lightsensor_baudrate_db);
+            this.Controls.Add(this.label_lightsensor_baudrate);
+            this.Controls.Add(this.btn_runtest);
+            this.Name = "MainForm";
+            this.Text = "Light Test Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_runtest;
+        private System.Windows.Forms.Label label_lightsensor_baudrate;
+        private System.Windows.Forms.ComboBox lightsensor_baudrate_db;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox lightsensor_databits_db;
+        private System.Windows.Forms.Label lightsensor_parity_label;
+        private System.Windows.Forms.ComboBox lightsensor_parity_db;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Daniels_LightTestApplication
+{
+    public partial class MainForm : Form
+    {
+
+        private SerialPort comport7_interior = new SerialPort();
+        private SerialPort comport8_exterior = new SerialPort();
+        private SerialPort comport2_lightsensor = new SerialPort();
+        
+
+
+        public void
+        OpenComport7()
+        {
+            if (comport7_interior.IsOpen)
+            {
+                comport7_interior.Close();
+            }
+            else
+            {
+                comport7_interior.PortName = "Comport 7";
+                comport7_interior.BaudRate = 38400;
+                comport7_interior.DataBits = 8;
+                comport7_interior.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1"); ;
+                comport8_exterior.Parity = (Parity)Enum.Parse(typeof(Parity), "None");
+                comport7_interior.Open();
+            }
+        }
+
+        public void
+        OpenComport8()
+        {
+            if (comport8_exterior.IsOpen)
+            {
+                comport8_exterior.Close();
+            }
+            else
+            {
+                comport8_exterior.PortName = "Comport 8";             
+                comport8_exterior.BaudRate = 38400;
+                comport8_exterior.DataBits = 8;
+                comport8_exterior.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1");
+                comport8_exterior.Parity   = (Parity)Enum.Parse(typeof(Parity), "None");
+                comport8_exterior.Open();
+            }
+        }
+
+        public void
+        OpenComport2()
+        // Open Comport 2 for any various light sensor
+        {
+            if (comport2_lightsensor.IsOpen)
+            {
+                comport2_lightsensor.Close();
+            }
+            else
+            {
+                comport2_lightsensor.PortName = "Comport 2";
+                comport2_lightsensor.BaudRate = int.Parse(lightsensor_baudrate_db.Text);
+                comport2_lightsensor.DataBits = int.Parse(lightsensor_databits_db.Text);
+                comport2_lightsensor.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1");
+                comport2_lightsensor.Parity   = (Parity)Enum.Parse(typeof(Parity), lightsensor_parity_db.Text);
+                comport2_lightsensor.Open();
+            }
+        }
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_runtest_Click(object sender, EventArgs e)
+        {
+            OpenComport7();
+            OpenComport8();
+            OpenComport2();
+
+            // Byte data of command that moves Unit 1 160 steps = 4.5 degrees
+            byte[] move_interior_160s = System.Text.Encoding.UTF8.GetBytes("@1N160");
+            byte[] u1_go = System.Text.Encoding.UTF8.GetBytes("@1G");
+
+            // Exterior Platform Loop
+            for (int i = 0; i <= 3200; i += 160)
+            {
+                // Interior Platform Loop
+                for (int ii = 0; i <= 6400; ii += 160)
+                {
+                    comport7_interior.Write(move_interior_160s, 0, 8);
+
+                }
+
+            
+            }
+  
+        
+        
+        
+        
+        
+        }
+    }
+}
